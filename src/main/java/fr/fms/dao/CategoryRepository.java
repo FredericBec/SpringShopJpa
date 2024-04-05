627b4150c34801085c2909c31f5b0eb5a306fa44
Frederic Bec <bec.frederic90@outlook.fr>
2024-04-05 11:20:39 +0200

    Add delete and update method

--- src/main/java/fr/fms/dao/CategoryRepository.java
+++ src/main/java/fr/fms/dao/CategoryRepository.java
@@ -2,7 +2,12 @@ package fr.fms.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.fms.entities.Category;
 
@@ -11,5 +16,12 @@ public interface CategoryRepository extends JpaRepository<Category, Long>{
 	public List<Category> findByOrderByNameAsc();
 	
 	public List<Category> findByOrderByNameDesc();
+	
+	public void deleteById(Long id);
+	
+	@Transactional
+	@Modifying
+	@Query("update Category C set C.name = :name where C.id = :id")
+	public void updateById(@Param("id") Long id, @Param("name") String name);
 
 }
